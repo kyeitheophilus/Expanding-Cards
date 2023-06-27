aea16fca04ef2ea22d14bec6b765de07e3b91a64
Theophilus Kyei <114838158+kyeitheophilus@users.noreply.github.com>
2023-06-27 05:30:31 +0000

    Add files via upload
    1st commit

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeAllClasses()
+    panel.classList.add('active')
+  })
+
+
+  function removeAllClasses() {
+    panels.forEach((panel) => {
+      panel.classList.remove('active')
+    })
+  }
+})
